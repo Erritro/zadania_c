f4c5728a3a9b518771784f93c6d6735a4225de16
Andrzej <andrzej.walczak@inbox.com>
2017-05-13 18:28:56 +0200

    Zadanie 2.4

--- 002.4.c
+++ 002.4.c
@@ -1,35 +1,22 @@
 #include <stdio.h>
 #include <math.h>
 
-//Oblicz pole powierzchni:
-//a) dowolnego trojkata [ 1/2 (bok a + bok b + bok c) ]
-//b) kwadratu (bok * bok)
-//c) prostokata (bok a * bok b)
-//d) kola, gdy znany jest jego promien
-// Dane sa odpowiednio boki lub promien
+//Oblicz przebyta droge w ruchu jednostajnym
+//jesli znane sa wartosci czasu i predkosci
 
 int main(){
 
-int bok_a, bok_b, bok_c, bok;
-float pole;
+int czas, predkosc;
 
-printf("Podaj dlugosc boku trojkata rownobocznego: ");
-scanf("%d", &bok);
-printf("Obwod trojkata rownobocznego wynosi %d \n\n", bok*3);
+printf("Obliczanie drogi w ruchu jednostajnym\n\n");
+printf("Podaj czas (w sekundach): ");
+scanf("%d", &czas);
+printf("\nPodaj predkosc (metrach na sekunde): ");
+scanf("%d", &predkosc);
+printf("\n");
+printf("Przebyta droga to: %d (metrow)\n\n", czas*predkosc);
 
-printf("Podaj dlugosc boku kwadratu: ");
-scanf("%d", &bok);
-printf("Obwod kwadratu wynosi %d \n\n", bok*4);
 
-printf("Podaj dlugosc pierwszego boku prostokata: ");
-scanf("%d", &bok1);
-printf("\nPodaj dlugosc drugiego boku prostokata: ");
-scanf("%d", &bok2);
-printf("Obwod prostokata wynosi %d \n\n", bok1*2+bok2*2);
-
-printf("Podaj promien okregu: ");
-scanf("%Lf", &promien);
-printf("Obwod okregu wynosi %Lf \n\n", 2*M_PI*promien);
 
 return 0;
 }
